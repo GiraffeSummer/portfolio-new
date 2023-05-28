370fd41b1874619f78204ede43b11c0a300f7342
GiraffeSummer <gidoselten@hotmail.com>
2023-05-28 16:56:30 +0200

    (temporarily) removed title skills

--- src/components/Skills.svelte
+++ src/components/Skills.svelte
@@ -10,7 +10,7 @@
 </script>
 
 <section id="skills">
-  <h2 class="prose">{data.title}</h2>
+  <!-- <h2 class="prose">{data.title}</h2> -->
   <div class="grid grid-flow-row-dense w-full gap-4 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
     {#each data.skills as skill}
       <div class="card w-96 bg-base-100 shadow-xl">
