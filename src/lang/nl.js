2a68936a72afbed30fb0a5b59a65be71dba06fc2
GiraffeSummer <gidoselten@hotmail.com>
2023-06-12 11:31:39 +0200

    fixed dutch sentences

--- src/lang/nl.js
+++ src/lang/nl.js
@@ -9,14 +9,14 @@ export default function (field) {
         'projects/nosource': 'Als er geen Github link staat',
 
         'projects/koala/name': 'Hikage / Koala',
-        'projects/koala/description': 'Hikage is een Discord bot waar ik sinds 2017 aan heb gewerkt (3 iteraties). Ik heb hem open source gehouden omdat ik veel moeite heb gestopt om het goed te structureren,\nen wou dat andere er van konden leren.',
+        'projects/koala/description': 'Hikage is een Discord bot waar ik sinds 2017 aan heb gewerkt (3 iteraties). Ik heb hem open source gehouden omdat ik veel moeite heb in gestopt om het goed te structureren,\nen wou dat andere er van konden leren.',
 
         'projects/pokednd/name': 'Pokednd',
-        'projects/pokednd/description': 'PokemonDND is een website die ik heb gemaakt als hulpmiddelen voor een Dungeons & Dragons Pokémon game die we speelden.',
+        'projects/pokednd/description': 'PokemonDND is een website die ik heb gemaakt met hulpmiddelen voor een Dungeons & Dragons Pokémon game die we speelden.',
 
         'projects/fabeltjeskrant/name': 'Fabeltjes Krant',
         'projects/fabeltjeskrant/description': 'Fabeltjes Krant is een lichtkrant die ik heb gemaakt voor DICHTERBIJ (de verbeelding Uden)\nSamen met een bewerkings tool om de informatie in de lichtkrant aan te passen.\n',
-        'projects/fabeltjeskrant/longtext': 'Dit was een van mijn eerste dynamische websites die ik heb gemaakt.\nDe lichtkrant liet zien wie er aanwezig was van het personeel, wat leuke nieuws berichten, en wat foto\'s als een diavoorstelling.\nIk heb deze opdracht gemaakt met de instelling dat ook de clienten bij dichterbij de informatie kunnen aanpassen.',
+        'projects/fabeltjeskrant/longtext': 'Dit was een van mijn eerste dynamische websites die ik heb gemaakt.\nDe lichtkrant liet zien welk personeel er aanwezig was, wat leuke nieuws berichten, en wat foto\'s als een diavoorstelling.\nIk heb deze opdracht gemaakt met de instelling dat ook de clienten bij dichterbij de informatie kunnen aanpassen.',
 
         'projects/drawapp/name': 'Drawapp',
         'projects/drawapp/description': 'Een app die meerdere users tegelijkertijd aan een pixelart laat werken.',
@@ -25,16 +25,16 @@ export default function (field) {
         'projects/gifmonkey/description': 'Een reactie gif deel platform met focus op een API voor bijvoorbeeld Discord bots',
 
         'projects/procons/name': 'Voor-/nadelen lijst',
-        'projects/procons/description': 'Een app om pro/con lijsten te maken, het kan ook andere lijsten, zoals todo.',
+        'projects/procons/description': 'Een app om pro/con lijsten te maken, je kan er andere lijsten mee maken zoals bijvoorbeeld todo.',
 
         'projects/pokeclicker/name': 'Pokemon clicker cheats',
         'projects/pokeclicker/description': 'Een script die UI cheats toevoegd aan de Pokemon clicker game, het was leuk om dit project te analyseren en dingen zo toe te voegen.',
 
         'projects/boardgame/name': 'Bordspel',
-        'projects/boardgame/description': "Een aanpasbaar bordspel, hij is erg modulair en aanpasbaar, je kan ook je eigen borden maken. Dit was erg anders dan mijn normale projecten.\nDit heeft ook een electron versie, dat was ook interessant om te maken.",
+        'projects/boardgame/description': "Een aanpasbaar bordspel, hij is erg modulair en aanpasbaar, je kan ook je eigen borden maken. Dit was anders dan mijn normale projecten.\nDit heeft ook een electron versie, dat was ook interessant om te maken.",
 
         'projects/friendlight/name': 'Vrienden lamp',
-        'projects/friendlight/description': "Een (physieke) vrienden lamp die synchroniseerd met de lamp van een vriend.",
+        'projects/friendlight/description': "Een (fysieke) vrienden lamp die synchroniseerd met de lamp van een vriend.",
         'projects/friendlight/longtext': 'Ik zag zo\'n zelfde idee online, en ik dacht dat ik het beter en goedkoper kon. dit was ook erg interessant met veel verschillende technieken.\n(De website is setup instructies, stuur een bericht voor meer info, of een demo)',
 
         'projects/portfolio/name': "Dit portfolio",
@@ -42,9 +42,9 @@ export default function (field) {
 
         'projects/shortener/name': "crri.pw",
         'projects/shortener/description': "Url verkorter and bestand deel site",
-        'projects/shortener/longtext': `Ik had een instructie video gekeken (van CodeGarden)\nOm nieuwe technieken te leren,
-         en ik dit interessant vond, hiervan heb ik grootendeels de url delen vandaan.
-         Later heb ik zelf de bestanden delen functie gemaakt omdat wetransfer niet gemakkelijk was vond ik.`,
+        'projects/shortener/longtext': `Ik had een instructie video gekeken (van CodeGarden)\nom nieuwe technieken te leren
+         en ik dit interessant vond. hier heb ik grootendeels het url verkorten vandaan.
+         Later heb ik zelf de bestanden delen functie gemaakt als alternatief voor WeTransfer.`,
 
         //skills general
         'skills/title': 'Skills',
